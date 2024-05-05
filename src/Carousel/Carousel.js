2414ac299533ceacdf6575b9fa59d8a63a0f03dd
Zalak Soni <zundavia@gmail.com>
2024-05-05 21:44:31 +0530

    Removed image, title change

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -7,7 +7,6 @@ export default function Carousel({ onClick }) {
     const images = [
         "https://images.unsplash.com/photo-1519748771451-a94c596fad67?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         "https://images.unsplash.com/photo-1595341888016-a392ef81b7de?q=80&w=2079&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        "https://en-pk.svestonwatches.com/cdn/shop/collections/Azadi_Banner2-03.jpg?v=1693829899",
         "https://www.diffeyewear.com/cdn/shop/articles/2022-DIFF-Web-Collection-Banner-Fall-New-Arrivals-1242x630_1024x1024.jpg?v=1666971266",
         "https://images.unsplash.com/photo-1571599164516-bacd34e651f7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         "https://images.unsplash.com/photo-1652911942944-572e532c8cf2?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
