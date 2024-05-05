eced75d8e5b221f62769243b0fd465a824a02939
Zalak Soni <zundavia@gmail.com>
2024-05-05 21:39:46 +0530

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDbTf1BjnrLMAFUs3Mr1URDzTKvjVwOQC8",
+    authDomain: "zyntra-8cdf4.firebaseapp.com",
+    projectId: "zyntra-8cdf4",
+    storageBucket: "zyntra-8cdf4.appspot.com",
+    messagingSenderId: "514956810773",
+    appId: "1:514956810773:web:1f27955052e6b0a2878c1c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
